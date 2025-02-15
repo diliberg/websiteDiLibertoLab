4e4fce72d097b08385de62d5ac0f416b2f560be5
diliberg <53035050+diliberg@users.noreply.github.com>
2025-02-15 14:00:47 +0000

    Update music-cognition.ts

--- src/data/research/music-cognition.ts
+++ src/data/research/music-cognition.ts
@@ -10,7 +10,7 @@ export const musicCognitionResearch: ResearchArea = {
   keyStudies: [
     {
       title: 'Encoding',
-      description: 'We study how the human brain encodes music. We utilise computational models, like IDyOM and music transformers, to determine how our brains process a note based on the preceding context, as well as studying the neural encoding of music timing and pitch. Our team is interested in shedding light on the relationship between melody statistics and music enjoyment, as well as investigating how different music genres are encoded in the human brain.',
+      description: 'We study how the human brain encodes music. We utilise computational models, like IDyOM and music transformers, to determine how our brains process a note based on the preceding context and to study the neural encoding of music timing and pitch. Our team is interested in shedding light on the relationship between melody statistics and music enjoyment, as well as investigating how different music genres are encoded in the human brain.',
       image: 'https://diliberg.github.io/websiteDiLibertoLabImages/picMusic.jpg',
       publications: [
         {
