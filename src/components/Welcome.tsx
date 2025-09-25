f9a680f9c6bff9a532a36e42cba30046263dc85a
YannickIskanderPeters <yannick.i.peters@gmail.com>
2025-09-25 11:17:02 +0100

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -27,11 +27,10 @@ const slides = [
 const newsItems = [
   {
     date: "September 2025", 
-    title: "New Preprints",
-    description: "Check out our new preprints on BioRxiv.",
+    title: "Martin's new preprint",
+    description: "Martin's new preprint on polyphonic musical perception is available on BioRxiv",
     image: "https://diliberg.github.io/websiteDiLibertoLabImages/BioRxiv.png",
-    link: "https://www.biorxiv.org/content/10.1101/2025.08.26.672294v1",
-    link: "https://www.biorxiv.org/content/10.1101/2025.09.23.674728v1" 
+    link: "https://www.biorxiv.org/content/10.1101/2025.08.26.672294v1"
   },
   {
     date: "July 2025", 
