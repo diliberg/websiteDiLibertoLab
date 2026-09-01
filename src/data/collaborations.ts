ea4c582d1c7fb5da310722d9063ea861bb689dff
YannickIskanderPeters <yannick.i.peters@gmail.com>
2026-09-01 08:09:15 +0000

    fix logos

--- src/data/collaborations.ts
+++ src/data/collaborations.ts
@@ -163,12 +163,12 @@ export const fundingLogos: FunderLogo[] = [
 export const researchhighlights: ResearchHighlight[] = [
   {
     name: "ARC-HUB",
-    logo: "https://marie-sklodowska-curie-actions.ec.europa.eu/themes/contrib/oe_theme/dist/ec/images/logo/positive/logo-ec--en.svg",
-    link: "https://diliberg.github.io/websiteDiLibertoLabImages/arc_hub.jpg"
+    logo: "https://diliberg.github.io/websiteDiLibertoLabImages/arc_hub.jpg",
+    link: "https://diliberto-lab.github.io/ARC-Hub-listening-study/"
   },
   {
     name: "Prodaptive",
     logo: "https://marie-sklodowska-curie-actions.ec.europa.eu/themes/contrib/oe_theme/dist/ec/images/logo/positive/logo-ec--en.svg",
-    link: "#prodaptive"
+    link: "prodaptive"
   }
 ];
